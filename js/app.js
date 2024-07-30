08c8810ab491120ac737993c49ca538662424f47
Betül Oran <116026974+betuloran@users.noreply.github.com>
2024-07-30 15:47:30 +0300

    app.js updated

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function setCookie(name,value,days){
+    var date=new Date();
+    date.setDate(date.getDate() + days);
+    document.cookie=name+ '=' + escape(value) + '; expires=' + date.toUTCString(); 
+    console.log('cookie' + name + 'created');
+
+}
+
+setCookie('prototurk',true,5);
